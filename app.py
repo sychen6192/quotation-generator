99da19e9b9c0b49878212fbbbbedd4c010d77d33
sychen6192 <qazxsw9295@gmail.com>
2021-07-16 11:50:01 +0800

    [feat] to Pdf

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from openpyxl import load_workbook
-import requests, datetime, pdfkit
-import pandas as pd
-
-
-headers = {'user-agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
-api_url = "https://data.gcis.nat.gov.tw/od/data/api/5F64D864-61CB-4D0D-8AD9-492047CC1EA6?$format=json&$filter=Business_Accounting_NO%20eq%2"
-datetime_dt = datetime.datetime.today()# 獲得當地時間
-datetime_str = datetime_dt.strftime("%Y/%m/%d")  # 格式化日期
+import requests, datetime, re
+from win32com import client
+import pythoncom
 
 
 def getCompanyInfo(id):
@@ -17,57 +12,75 @@ def getCompanyInfo(id):
         return '404'
 
 
-wb = load_workbook('uutd5-xunqs.xlsx')
-sheet = wb['报价单']
+def getQuotation(data):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
+    api_url = "https://data.gcis.nat.gov.tw/od/data/api/5F64D864-61CB-4D0D-8AD9-492047CC1EA6?$format=json&$filter=Business_Accounting_NO%20eq%2"
+    datetime_dt = datetime.datetime.today()  # 獲得當地時間
+    datetime_str = datetime_dt.strftime("%Y/%m/%d")  # 格式化日期
 
-sheet['G3'] = datetime_str
-# deadline
-dateB = int(input('報價有效天數：'))
-sheet['G8'] = (datetime_dt + datetime.timedelta(days=dateB)).strftime("%Y/%m/%d")
-# name
-name = input('報價客戶姓名：')
-sheet['B9'] = f'姓名：{name}'
-cId = input('報價公司統編：')
-res = getCompanyInfo(cId)[0]
-sheet['B10'] = f'公司名稱：'+res['Company_Name']
-sheet['B11'] = f'統一編號：'+res['Business_Accounting_NO']
-sheet['B12'] = f'公司地址：'+res['Company_Location']
-cPhone = input('報價公司電話：')
-sheet['B13'] = f'公司電話：'+cPhone
-ps = input('備註：')
-if ps:
-    sheet['C16'] = ps
-else:
-    sheet['C16'] = '無'
+    wb = load_workbook('C:\\quotation-generator\\uutd5-xunqs.xlsx')
+    sheet = wb['报价单']
 
-product = []
-while True:
-    pp = input('請輸入品項 (商品, 數量, 單價)，結束請按b:')
-    if pp[0] == 'b':
-        break
-    product.append(pp.split(','))
-
-tax = input('含稅(y/n)：')
-for i in range(24, 24+len(product)):
-    sheet.merge_cells(f'C{i}:D{i}')
-    sheet['B'+str(i)] = product[i-24][1]
-    sheet['C'+str(i)] = product[i-24][0]
-    sheet['E'+str(i)] = product[i-24][2]
-    if tax == 'n':
-        sheet['F' + str(i)] = 'T'
+    sheet['G3'] = datetime_str
+    # deadline
+    dateB = data.get('vday')
+    sheet['G8'] = (datetime_dt + datetime.timedelta(days=int(dateB))).strftime("%Y/%m/%d")
+    # name
+    name = data.get('cname')
+    sheet['B9'] = f'姓名：{name}'
+    cId = data.get('taxid')
+    res = getCompanyInfo(cId)[0]
+    sheet['B10'] = f'公司名稱：'+res['Company_Name']
+    sheet['B11'] = f'統一編號：'+res['Business_Accounting_NO']
+    sheet['B12'] = f'公司地址：'+res['Company_Location']
+    cPhone = data.get('cphone')
+    sheet['B13'] = f'公司電話：'+cPhone
+    ps = data.get('note')
+    if ps:
+        sheet['C16'] = ps
     else:
-        sheet['F' + str(i)] = ''
+        sheet['C16'] = '無'
 
-name = input('售貨員：')
-sheet['B20'] = name
-delivery = input('發貨日+n：')
-sheet['C20'] = (datetime_dt + datetime.timedelta(days=int(delivery))).strftime("%Y/%m/%d")
-delivery_way = input('發貨方式：')
-sheet['D20'] = delivery_way
-cash = input('付款方式：')
-sheet['F20'] = cash
+    pp = data.get('product').split('\r\n')
+    products = [re.sub(r'[,\s]', '<sep>',p) for p in pp]
+    products = [product.split('<sep>') for product in products]
 
-wb.save('out.xlsx')
-df = pd.read_excel("out.xlsx")
-df.to_html("out.html")
-pdfkit.from_file("out.html", "out.pdf")
+    tax = data.get('tax')
+    for i in range(24, 24+len(products)):
+        sheet.merge_cells(f'C{i}:D{i}')
+        sheet['B'+str(i)] = products[i-24][1]
+        sheet['C'+str(i)] = products[i-24][0]
+        sheet['E'+str(i)] = products[i-24][2]
+        sheet['G' + str(i)] = '=B'+str(i)+'*E'+str(i)
+        if tax == 'n':
+            sheet['F' + str(i)] = 'T'
+        else:
+            sheet['F' + str(i)] = ''
+
+    name = data.get('seller')
+    sheet['B20'] = name
+    delivery = data.get('dday')
+    sheet['C20'] = delivery.replace('-', '/')
+    delivery_way = data.get('delivery')
+    sheet['D20'] = delivery_way
+    cash = data.get('cash')
+    sheet['F20'] = cash
+    wb.save('C:\\quotation-generator\\out.xlsx')
+
+    # to PDF
+    fileName = res['Company_Name']+'報價單'+datetime_dt.strftime("%m%d")
+    pythoncom.CoInitialize()
+    app = client.DispatchEx("Excel.Application")
+    app.Interactive = False
+    app.Visible = False
+    Workbook = app.Workbooks.Open('C:\\quotation-generator\\out.xlsx')
+    try:
+        Workbook.ActiveSheet.ExportAsFixedFormat(0, f'C:\\quotation-generator\\{fileName}.pdf')
+    except Exception as e:
+        print("Failed to convert in PDF format.Please confirm environment meets all the requirements and try again")
+        print(str(e))
+    finally:
+        Workbook.Close(False)
+        app.Quit()
+    return fileName
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, redirect, url_for, render_template,request
+from flask import Flask, redirect, url_for, render_template,request, send_file
+from app import getQuotation
+import os
 from flask_bootstrap import Bootstrap
 from flask_datepicker import datepicker
 
@@ -7,11 +9,6 @@ Bootstrap(app)
 datepicker(app)
 
 @app.route("/", methods=["POST","GET"])
-def hello():
-    return render_template("hello.html")
-
-
-@app.route("/quotation", methods=["POST","GET"])
 def login():
     return render_template("form.html")
 
@@ -19,8 +16,14 @@ def login():
 @app.route("/generate", methods=["POST","GET"])
 def generate():
     if request.method == 'POST':
-        return 'Hello ' + request.form.get('name')
+        filename = getQuotation(request.form)
+        return render_template("gen.html", filename=filename)
 
+@app.route('/download/<filename>')
+def downloadFile (filename):
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = f'C:\\quotation-generator\\{filename}.pdf'
+    return send_file(path, as_attachment=True)
 
 if __name__ =="__main__":
     app.run(debug=True)
